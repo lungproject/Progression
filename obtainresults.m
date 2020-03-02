09cd435860422410ecc40e541920004e1178970c
Mavis <53061877+lungproject@users.noreply.github.com>
2020-03-02 08:44:53 -0500

    Update obtainresults.m

--- obtainresults.m
+++ obtainresults.m
@@ -2,17 +2,16 @@ close all
 clear
 clc
 
-load('./alldata/pathallpatchc.mat');
+load('./alldata/pathallpatchcsmall.mat');
 
 % 
 trainfeature = load('./Results/predicttrain.txt');
 testfeature = load('./Results/predicttest.txt');
-valfeature = load('./Results/predictval.txt');
+
 
 
 trainfeature = trainfeature(:,2);
 testfeature = testfeature(:,2);
-valfeature = valfeature(:,2);
 
 trainnum = unique(trainGroups);
 for i=1:length(trainnum)
@@ -41,28 +40,3 @@ evetrain = EvaluationModel(trainpp,traintrue,1,1)
 evetest = EvaluationModel(testpp,testtrue,1,1)
 
 
-valnum = unique(valGroups);
-for i=1:length(valnum)
-    ind = find(valGroups==valnum(i));
-    temp1 = valfeature(ind);
-    valpp(i,:) = mean(temp1);
-    valtrue(i,:)=mean(vallabels(ind));
-    
-
-    ss=valpaths{ind(1)};
-    ins = strfind(ss,'/'); ins2 = strfind(ss,'.'); 
-    valname{i} = ss(ins(end-1)+1:ins2(end)-1);
-    
-
-end
-valname=valname';
-eveval = EvaluationModel(valpp,valtrue,1,1)
-
-evetogether = EvaluationModel([testpp;valpp],[testtrue;valtrue],1,1)
-
-
- [~,cutoff1,~,~] = AllAuc (trainpp,traintrue)
-
- [~,cutoff2,~,~] = AllAuc (testpp,testtrue) 
- 
- [~,cutoff3,~,~] = AllAuc (valpp,valtrue) 
